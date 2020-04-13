14c25f0f61817355b8bea2b6fa4efa4ae3b7a038
Julisitta <37798545+Julisitta@users.noreply.github.com>
2020-04-13 13:25:19 +0300

    Calender creation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,12 @@
+'use strict'
+
 window.onload = function() {
-    document.querySelector('.cause').addEventListener('click', fTabs);
-    document.querySelector('.cancel').addEventListener('click', close);
-    document.querySelector('.send').addEventListener('click', send);
-    //localStorage.clear();
-    
+    document.querySelector('.cause').addEventListener('click', openPopup);
+    document.querySelector('.cancel').addEventListener('click', closePopup);
+    document.querySelector('.send').addEventListener('click', sendForm);
 }
 
-function fTabs() {
+function openPopup() {
     document.getElementById('title').value = null;
     document.getElementById('name').value = null;
     document.getElementById('lent').value = null;
@@ -14,43 +14,52 @@ function fTabs() {
     document.querySelector(".save").style.display = "none";
     document.querySelector(".send").style.display = " inline-block";
     document.getElementById("pop-up").style.display = "block";
-    document.getElementById("gray").style.display = "block";
 }
 
-function close() {
+function editingBook(book, newLi, inputTitle, inputName, inputLent, inputUntil, counter) {
+    document.getElementById('title').value = book.title;
+    document.getElementById('name').value = book.name;
+    document.getElementById('lent').value = book.lent;
+    document.getElementById('until').value = book.until;
+    let numberOfBook = Number(counter.innerHTML);
+    document.getElementById("delete-books").style.display = "flex";
+    document.querySelector(".send").style.display = "none";
+    document.querySelector(".save").style.display = "inline-block";
+    document.querySelector('.save').onclick = function() {saveChanges(newLi, inputTitle, inputName, inputLent, inputUntil,numberOfBook-1)};
+    document.getElementById("pop-up").style.display = "block";
+}
+
+function closePopup() {
     document.getElementById("pop-up").style.display = "none";
-    document.getElementById("gray").style.display = "none";
+    if (document.getElementById("tcal") != null) {document.getElementById("tcal").style.visibility = "hidden"};
+    if (document.querySelector(".tcalActive") != null) {document.querySelector(".tcalActive").style.background = "url('img/cal.gif') 100% 50% no-repeat"};
+    document.getElementById("delete-input").checked = false;
 }
 
 const mainUl = document.getElementById("book-list");
 
-function send() {
+function sendForm() {
     let title = document.getElementById('title').value;
     let name = document.getElementById('name').value;
     let lent = document.getElementById('lent').value;
     let until = document.getElementById('until').value;
     let newBook = new Book(title, name, lent, until);
-    console.log("пушим книгу")
     pushBook(newBook);
-    close();
+    closePopup();
 }
 
-function save(newLi, inputTitle, inputName, inputLent, inputUntil, numberOfBook) {
+function saveChanges(newLi, inputTitle, inputName, inputLent, inputUntil, numberOfBook) {
     let bookArray = JSON.parse(localStorage.getItem('books'));
     if (document.getElementById("delete-input").checked) {
-       
-        console.log("delete", newLi);
         newLi.remove();
-        bookArray.splice(bookArray, numberOfBook+1);
+        bookArray.splice(numberOfBook, 1);
         document.getElementById("delete-input").checked = false;
-
     } else {
         let title = document.getElementById('title').value;
         let name = document.getElementById('name').value;
         let lent = document.getElementById('lent').value;
         let until = document.getElementById('until').value;
 
-        console.log(bookArray)
         inputTitle.innerHTML = title;
         inputName.innerHTML = name;
         inputLent.innerHTML = lent;
@@ -60,11 +69,10 @@ function save(newLi, inputTitle, inputName, inputLent, inputUntil, numberOfBook)
         bookArray[numberOfBook].name = name;
         bookArray[numberOfBook].lent = lent;
         bookArray[numberOfBook].until = until;
-        console.log(title);
-        window.location.reload();
     }
     localStorage.setItem('books', JSON.stringify(bookArray));
-    close();
+    closePopup();
+    window.location.reload();
 }
 
 
@@ -82,25 +90,30 @@ localStorage.setItem('books', JSON.stringify(bookList));
 
 let pushBook = function(newBook) {
     bookList.push(newBook);
-    
     localStorage.setItem('books', JSON.stringify(bookList));
     bookList = JSON.parse(localStorage.getItem('books'));
     liMaker(newBook);
 }
 
-
-    
 let i=1;
 const liMaker = (book) => {
     let newLi = document.createElement('li');
     let inputTitle = document.createElement('p');
     let inputName = document.createElement('p');
     let inputLent = document.createElement('p');
+    
     let inputUntil = document.createElement('p');
     let counter = document.createElement('p');
     let button = document.createElement('button');
+
+    let inputTitleinputName = document.createElement('div');
+    let inputLentinputUntil = document.createElement('div');
+    inputTitleinputName.append(inputTitle, inputName);
+    inputLentinputUntil.append(inputLent, inputUntil);
+    inputTitleinputName.className = "div-for-name";
+    inputLentinputUntil.className = "div-for-lent";
     
-    newLi.append(counter, inputTitle, inputName, inputLent, inputUntil, button);
+    newLi.append(counter, inputTitleinputName, inputLentinputUntil, button);
 
     newLi.className = "list";
     inputTitle.className = "new-title";
@@ -109,23 +122,9 @@ const liMaker = (book) => {
     inputUntil.className = "new-until";
     counter.className = "new-counter";
     button.className = "new-button";
-    
     button.onclick = function(){
-        document.getElementById('title').value = book.title;
-        document.getElementById('name').value = book.name;
-        document.getElementById('lent').value = book.lent;
-        document.getElementById('until').value = book.until;
-        numberOfBook = Number(counter.innerHTML);
-        document.getElementById("delete-books").style.display = "flex";
-        document.querySelector(".send").style.display = "none";
-        document.querySelector(".save").style.display = "inline-block";
-        document.querySelector('.save').onclick = function() {save(newLi, inputTitle, inputName, inputLent, inputUntil,numberOfBook-1)};
-        document.getElementById("pop-up").style.display = "block";
-        document.getElementById("gray").style.display = "block";
-        console.log(typeof numberOfBook);
+        editingBook(book, newLi, inputTitle, inputName, inputLent, inputUntil, counter);
     }
-
-    console.log("новый li создан");
 
     inputTitle.innerHTML = book.title;
     inputName.innerHTML = book.name;
@@ -133,12 +132,9 @@ const liMaker = (book) => {
     inputUntil.innerHTML = book.until;
     button.innerHTML = "  ";
     counter.innerHTML = i;
-
     mainUl.append(newLi);
     i++;
-    console.log(bookList.length);
     document.getElementById("lent-length").innerHTML = "You have lent " +  bookList.length + " books to friend";
-
 }
 
 bookList.forEach(book => {
